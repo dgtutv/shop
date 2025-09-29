90fe8128b80de2587555c27dcfef6460762a6b82
Daniel Todd <dgtutv@gmail.com>
2025-09-29 13:30:55 -0700

    Generated basic layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Shop from './pages/shop.jsx'
+import CartPage from './pages/cart.jsx'
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "shop",
+    element: <Shop />,
+  },
+  {
+    path: "cart",
+    element: <Cart />,
+  },
+]);
+
+//Header and footer should go in here, around the router
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
