34d7e3ce2d2d5b01cab1aa260b3046e48303391f
Daniel Todd <dgtutv@gmail.com>
2025-09-30 14:21:02 -0700

    Moved layout css to another file for cleaner code

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,50 +1,13 @@
 import React from 'react';
 import { NavLink, useLocation } from 'react-router';
+import "./layout.css"
 
 const Header = () => {
     const location = useLocation();
-    
-    const headerStyle = {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "1rem 2rem",
-        backgroundColor: "#181818ff",
-        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.86)",
-        margin: "0"
-    }
-
-    const navStyle = {
-        display: "flex",
-        gap: "2rem",
-        alignItems: "center"
-    }
-
-    const linkStyle = {
-        textDecoration: "none",
-        color: "#e4e4e4",
-        fontWeight: "500",
-        padding: "0.5rem 1rem",
-        borderRadius: "2px",
-        transition: "all 0.2s ease-in-out"
-    }
-
-    const activeLinkStyle = {
-        ...linkStyle,
-        backgroundColor: "#202020ff",
-        fontWeight: "600",
-        borderBottom: "2px solid yellow",
-        color: "yellow"
-    }
-
-    const titleStyle = {
-        margin: 0, 
-        fontSize: "1.5rem"
-    }
 
     return (
-        <header style={headerStyle}>
-            <h1 style={titleStyle}>
+        <header>
+            <h1>
                 {(() => {
                     switch (location.pathname) {
                         case "/shop":
@@ -56,22 +19,22 @@ const Header = () => {
                     }
                 })()}
             </h1>
-            <nav style={navStyle}>
+            <nav>
                 <NavLink 
                     to="/" 
-                    style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}
+                    className={({ isActive }) => isActive ? "activeLinkStyle" : "linkStyle"}
                 >
                     Home
                 </NavLink>
                 <NavLink 
                     to="/shop" 
-                    style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}
+                    className={({ isActive }) => isActive ? "activeLinkStyle" : "linkStyle"}
                 >
                     Shop
                 </NavLink>
                 <NavLink 
                     to="/cart" 
-                    style={({ isActive }) => isActive ? activeLinkStyle : linkStyle}
+                    className={({ isActive }) => isActive ? "activeLinkStyle" : "linkStyle"}
                 >
                     Cart
                 </NavLink>
