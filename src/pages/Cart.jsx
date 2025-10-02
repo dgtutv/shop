39381852cef62b7a75b1d600eb0b6a021e9618f0
Daniel Todd <dgtutv@gmail.com>
2025-10-01 19:44:43 -0700

    Initial implementation

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-
-//Retrieve and write card via localStorage
+import { useCart } from '../contexts/CartContext';
+import CartProduct from '../components/CartProduct';
 
 const CartPage = () => {
+    const { products, productCounts, loading, addToCart, removeFromCart, saveForLater } = useCart();
+
+    const handleCartChange = (product, action) => {
+        if (action === 'add') {
+            addToCart(product);
+        }
+        else if (action === "remove") {
+            removeFromCart(product);
+        }
+    };
+
+    const removeAll = (product) => {
+        if (productCounts[product.id - 1] === 0) {
+            return;
+        }
+        else {
+            removeFromCart(product);
+            removeAll(product);
+        }
+    }
+
+    if (loading) {
+        return <p>Loading products...</p>;
+    }
     return (
         <div>
             <h1>Cart Page</h1>
             <p>You are currently on the Cart page.</p>
+            <CartProduct key={products[0].id} product={products[0]} removeAll={removeAll} saveForLater={saveForLater} editByOne={handleCartChange} productCount={productCounts[0]} />
         </div>
     );
 };
